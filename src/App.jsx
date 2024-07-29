ffadba7cb56ec42c9b5a2fe6cea8d65151db49e6
yu <43624848+leey611@users.noreply.github.com>
2024-07-28 21:30:24 -0400

    update slides styles

--- src/App.jsx
+++ src/App.jsx
@@ -32,36 +32,47 @@ function App() {
 
   return (
     <article id="article_wrapper">
-      <SimpleSlide viewportPosition={20}>
-        <h1>0 haunt hunt</h1>
+      <SimpleSlide viewportPosition={15}>
+        <h1>haunt hunt</h1>
+        <h2 id="subtitle">an indie, custom arcade game</h2>
       </SimpleSlide>
       <SimpleSlide viewportPosition={100}>
-        <p>1 haunt hunt is a custom, two player arcade game where one player takes on the role of a detective, while the other embodies a mischievous ghost.</p>
+        <p>haunt hunt is a custom, two player arcade game where one player takes on the role of a detective, while the other embodies a mischievous ghost.</p>
       </SimpleSlide>
       <SimpleSlide viewportPosition={200}>
-        <p>2 detective has a radar to get the ghost's location, a _ to set a trap to slow down the ghost, _ to extend field of view, _ to reveal the front area. </p>
+        <p>detective has a radar to get the ghost's location, a _ to set a trap to slow down the ghost, _ to extend field of view, _ to reveal the front area. </p>
       </SimpleSlide>
       <SimpleSlide viewportPosition={300}>
-        <p>3 ghost can use _ to place a moving duplicate, if a duplicate is killed, detective's movements get disturbed for a few seconds, _ to be immune and invisible from the detective's light, and able to go through the walls for 5 seconds, _ to teleport to a random location.</p>
+        <p>ghost can use _ to place a moving duplicate, if a duplicate is killed, detective's movements get disturbed for a few seconds, _ to be immune and invisible from the detective's light, and able to go through the walls for 5 seconds, _ to teleport to a random location.</p>
       </SimpleSlide>
       <SimpleSlide viewportPosition={400}>
-        <h2>4 acrylic top</h2>
+        <h2>acrylic top</h2>
         <p>on the top of the arcade is an laser cut acrylic that serves as a screen protector for the external monitor.</p>
       </SimpleSlide>
       <SimpleSlide viewportPosition={500}>
-        <h2>6 controls</h2>
+        <h2>controls</h2>
         <p>buttons and joysticks are located on the platforms on the sides.</p>
       </SimpleSlide>
       <SimpleSlide viewportPosition={600}>
-        <h2>6 speaker</h2>
+        <h2>speaker</h2>
         <p>the hole on the front side of the cabinet is where the souund speaker is put.</p>
       </SimpleSlide>
       <SimpleSlide viewportPosition={700}>
-        <h2>7 storage</h2>
+        <h2>storage</h2>
         <p>on the back side, a storage is available to put the laptop.</p>
       </SimpleSlide>
       <SimpleSlide viewportPosition={800}>
-        <h2>credit</h2>
+        <h2>credits</h2>
+        <ul>
+          <li>eric shen - game development</li>
+          <li>johnny zhou - physical computing, game design</li>
+          <li>yu lee - fabrication, documentation</li>
+        </ul>
+        <h2>special thanks</h2>
+        <ul id='special_thanks'>
+          <li>@newarcadeitp</li>
+          <li>@wondervillenyc</li>
+        </ul>
       </SimpleSlide>
       <Scene />
     </article>
